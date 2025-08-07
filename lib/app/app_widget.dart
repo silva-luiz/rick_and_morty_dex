464d87f2678bd71e55c05d30b1bb9cc19d2b52ac
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-08-07 15:09:18 -0300

    refact: remove debug banner

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -11,6 +11,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Rick and Morty Dex',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
