f983ff750421b553480ad9f4485e3af1fb1c0adb
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-08-07 01:10:35 -0300

    feat: add Colors file

---
+++ lib/modules/shared/colors/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF29FF01);
+  static const Color secondary = Color(0xFF1DA9C2);
+  static const Color background = Color.fromARGB(255, 28, 45, 63);
+  static const Color cardBackground = Color.fromARGB(255, 14, 26, 37);
+  static const Color primaryText = Color(0xFFFFFFFF);
+  static const Color secondaryText = Color(0xFFCCCCCC);
+  static const Color blackText = Color(0xFF000000);
+  static const Color details = Color(0xFFFF4FA3);
+  static const Color success = Color(0xFF9EFFA1);
+  static const Color warning = Color.fromARGB(255, 238, 176, 4);
+  static const Color error = Color(0xFFFF4E42);
+}
